1350dd576d6c2584fa29ea3f37b3e549a11ffac8
PauloUbirajara <ubirajara3@hotmail.com>
2022-04-27 17:18:08 -0300

    Frontend básico

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,16 +1,19 @@
-import { removeWritespaces } from '../services/backend';
+import { removeWhitespaces } from '../services/backend';
 import './MainPage.css';
 
 export default function MainPage() {
 	const text = '    eu sou   uma busca  zudaa  ouoooooo     uuuu !! ';
 	console.log(`[${text}]`);
-	console.log(`[${removeWritespaces(text)}]`);
+	console.log(`[${removeWhitespaces(text)}]`);
 
 	return (
 		<div className="main">
-			<input type="text" id="query" placeholder="Insira!" />
-			<button id="parse-query">Receba!</button>
-			<div id="results">
+			<div className="user-input">
+				<h2>Processador de consultas</h2>
+				<input type="text" id="query" placeholder="Insira!" />
+				<button id="parse-query">Receba!</button>
+			</div>
+			<div class="results">
 				<h2>Resultado da consulta</h2>
 				<div id="results-body"></div>
 			</div>
