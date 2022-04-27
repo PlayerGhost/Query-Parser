be26a5decfa78c0f480d419fe16a97d145999127
PauloUbirajara <ubirajara3@hotmail.com>
2022-04-27 14:29:51 -0300

    Criar JSON contendo relações de tabelas (WIP)

--- src/services/backend.js
+++
@@ -1,4 +0,0 @@
-export const removeWhitespaces = (text) => text.split(' ').filter((x) => x);
-
-// TODO Passar texto para maiúsculo antes de verificar pelo regex
-const operatorRegex = /(=|<=|<>|>=|<|>|AND|IN|NOT IN)/;
