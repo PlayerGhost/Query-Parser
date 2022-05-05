591a2735cca7e8783fb8c260e9fd72925c2f404b
PauloUbirajara <ubirajara3@hotmail.com>
2022-05-05 02:50:03 -0300

    Indentar código

--- src/helpers/regex.js
+++ src/helpers/regex.js
@@ -1,55 +1,51 @@
 const examples = [
-    `SELECT NOME, DATANASCIMENTO, DESCRICAO, SALDOINICIAL FROM USUARIO JOIN CONTAS ON USUARIO.IDUSUARIO = CONTAS.USUARIO_IDUSUARIO WHERE SALDOINICIAL >=235 AND UF ='CE' AND CEP <> '62930000';`,
-    `SELECT IDUSUARIO, NOME, DATANASCIMENTO, DESCRICAO, SALDOINICIAL, UF, DESCRICAO FROM USUARIO JOIN CONTAS ON USUARIO.IDUSUARIO = CONTAS.USUARIO_IDUSUARIO JOIN TIPOCONTA ON TIPOCONTA.IDTIPOCONTA = CONTAS.TIPOCONTA_IDTIPOCONTA ;`,
-    'SELECT * FROM USUARIO;',
-    'SELECT *A FROM USUARIO;',
-    'SELECT A, FROM USUARIO;',
-    'SELECT A,A FROM USUARIO;',
-    'SELECT A,A FROM USUARIO;',
-    ' FROM USUARIO;',
-    '* FROM USUARIO;',
-    '* FROM USUARIO;',
-    'SELECT * FROM USUARIO WHERE id = 1;',
-    'SELECT * FROM USUARIO WHERE id <> 1;',
-    'SELECT * FROM USUARIO WHERE id > 1;',
-    'SELECT * FROM USUARIO WHERE id < 1;',
-    'SELECT * FROM USUARIO WHERE id >= 1;',
-    'SELECT * FROM USUARIO WHERE id <= 1;',
-    'SELECT * FROM USUARIO WHERE id <= 1 AND id >= 3;',
-    'SELECT * FROM USUARIO WHERE id <= 1 AND ;',
-    'SELECT * FROM USUARIO WHERE id <= 1 AND;',
-    'SELECT * FROM USUARIO WHERE id <= ;',
-    'SELECT FROM USUARIO WHERE id <= ;',
-    'SELECT * FROM USUARIO WHERE;',
-    `SELECT * FROM USUARIO WHERE user = 'renatasasaso sdasasasaaasds addsd' AND X = 1;`,
-    `SELECT * FROM USUARIO WHERE user = 'renato_ddsdasddsddasdsds';`,
-    `SELECT NOME, DATANASCIMENTO, DESCRICAO, SALDOINICIAL FROM USUARIO JOIN CONTAS ON USUARIO.IDUSUARIO = CONTAS.USUARIO_IDUSUARIO WHERE user = 'renato vdsdsadsddasdsdv SDSAD' DSD;`,
-    `SELECT * FROM USUARIO WHERE user = 'renato vdsdsadsddasdsdv SDSAD' DSD;`,
-    `SELECT * FROM USUARIO WHERE user = 'renato' AND;`,
-    `SELECT * FROM USUARIO WHERE user='renato';`
-]
-
+	`SELECT NOME, DATANASCIMENTO, DESCRICAO, SALDOINICIAL FROM USUARIO JOIN CONTAS ON USUARIO.IDUSUARIO = CONTAS.USUARIO_IDUSUARIO WHERE SALDOINICIAL >=235 AND UF ='CE' AND CEP <> '62930000';`,
+	`SELECT IDUSUARIO, NOME, DATANASCIMENTO, DESCRICAO, SALDOINICIAL, UF, DESCRICAO FROM USUARIO JOIN CONTAS ON USUARIO.IDUSUARIO = CONTAS.USUARIO_IDUSUARIO JOIN TIPOCONTA ON TIPOCONTA.IDTIPOCONTA = CONTAS.TIPOCONTA_IDTIPOCONTA ;`,
+	'SELECT * FROM USUARIO;',
+	'SELECT *A FROM USUARIO;',
+	'SELECT A, FROM USUARIO;',
+	'SELECT A,A FROM USUARIO;',
+	'SELECT A,A FROM USUARIO;',
+	' FROM USUARIO;',
+	'* FROM USUARIO;',
+	'* FROM USUARIO;',
+	'SELECT * FROM USUARIO WHERE id = 1;',
+	'SELECT * FROM USUARIO WHERE id <> 1;',
+	'SELECT * FROM USUARIO WHERE id > 1;',
+	'SELECT * FROM USUARIO WHERE id < 1;',
+	'SELECT * FROM USUARIO WHERE id >= 1;',
+	'SELECT * FROM USUARIO WHERE id <= 1;',
+	'SELECT * FROM USUARIO WHERE id <= 1 AND id >= 3;',
+	'SELECT * FROM USUARIO WHERE id <= 1 AND ;',
+	'SELECT * FROM USUARIO WHERE id <= 1 AND;',
+	'SELECT * FROM USUARIO WHERE id <= ;',
+	'SELECT FROM USUARIO WHERE id <= ;',
+	'SELECT * FROM USUARIO WHERE;',
+	`SELECT * FROM USUARIO WHERE user = 'renatasasaso sdasasasaaasds addsd' AND X = 1;`,
+	`SELECT * FROM USUARIO WHERE user = 'renato_ddsdasddsddasdsds';`,
+	`SELECT NOME, DATANASCIMENTO, DESCRICAO, SALDOINICIAL FROM USUARIO JOIN CONTAS ON USUARIO.IDUSUARIO = CONTAS.USUARIO_IDUSUARIO WHERE user = 'renato vdsdsadsddasdsdv SDSAD' DSD;`,
+	`SELECT * FROM USUARIO WHERE user = 'renato vdsdsadsddasdsdv SDSAD' DSD;`,
+	`SELECT * FROM USUARIO WHERE user = 'renato' AND;`,
+	`SELECT * FROM USUARIO WHERE user='renato';`
+];
 
 //const chars2 = "([A-ZÇÃÚ0-9]+)(\\.?_?[A-ZÇÃÚ0-9]+)*"
 //const chars2 = "([A-Za-zçãú0-9]+)(\\.?_?[A-Za-zçãú0-9]+)*"
 
 //const chars2 = "A-Za-zÀ-ÖØ-öø-ÿ0-9"
 
-const chars2 = "([A-ZÇÃÚ0-9._]+)"
+const chars2 = '([A-ZÇÃÚ0-9._]+)';
 
-const select = `SELECT ([*]|(${chars2}))(,\\s?${chars2})* FROM ${chars2}`
+const select = `SELECT ([*]|(${chars2}))(,\\s?${chars2})* FROM ${chars2}`;
 
-const where = `WHERE ${chars2}\\s?((=|<=|<>|>=|<|>)\\s?((${chars2})|'(${chars2}\\s?)+'))( AND ${chars2}\\s?((=|<=|<>|>=|<|>)\\s?((${chars2})|'(${chars2}\\s?)+')))*`
+const where = `WHERE ${chars2}\\s?((=|<=|<>|>=|<|>)\\s?((${chars2})|'(${chars2}\\s?)+'))( AND ${chars2}\\s?((=|<=|<>|>=|<|>)\\s?((${chars2})|'(${chars2}\\s?)+')))*`;
 
-const join = `JOIN ${chars2} ON ${chars2}\\s?=\\s?${chars2}`
+const join = `JOIN ${chars2} ON ${chars2}\\s?=\\s?${chars2}`;
 
-const query = `^${select}\\s?(( ${where})?|( ${join})+?( ${where})?)?\\s?;$`
+const query = `^${select}\\s?(( ${where})?|( ${join})+?( ${where})?)?\\s?;$`;
 //const query = `^${select}\\s?( ${where})\\s?;$`
 
 export const regex = new RegExp(query);
-
-
-
 
 /*const regexArray = [
     /,,+/i,                                          // Vírgulas seguidas
@@ -62,13 +58,10 @@ export const regex = new RegExp(query);
     /WHERE \w\s(=|<>|<|>|<=|>=)\s?(?!\w+)/i,         // Operadores de comparação sem segundo valor
 ]*/
 
+examples.forEach((text) => {
+	console.log(`Texto: ${text.toUpperCase()}`);
+	console.log(`Resultados: [${text.toUpperCase().match(regex)}]`);
+	console.log('--------------------------------------------------------');
+});
 
-
-examples.forEach(text => {
-    console.log(`Texto: ${text.toUpperCase()}`)
-    console.log(`Resultados: [${text.toUpperCase().match(regex)}]`)
-    console.log("--------------------------------------------------------")
-})
-
-
-console.log(regex)
+console.log(regex);
--- src/helpers/relationalAlgebra.js
+++ src/helpers/relationalAlgebra.js
@@ -1,71 +1,72 @@
-const teste = "SELECT IDUSUARIO, NOME, DATANASCIMENTO, DESCRICAO, SALDOINICIAL, UF, DESCRIÇÃO FROM USUARIO JOIN CONTAS ON USUARIO.IDUSUARIO = CONTAS.USUARIO_IDUSUARIO JOIN TIPOCONTA ON TIPOCONTA.IDTIPOCONTA = CONTAS.TIPOCONTA_IDTIPOCONTA WHERE SALDOINICIAL < 3000 AND UF = 'CE' AND DESCRIÇÃO <> 'CONTA CORRENTE'"
+const teste =
+	"SELECT IDUSUARIO, NOME, DATANASCIMENTO, DESCRICAO, SALDOINICIAL, UF, DESCRIÇÃO FROM USUARIO JOIN CONTAS ON USUARIO.IDUSUARIO = CONTAS.USUARIO_IDUSUARIO JOIN TIPOCONTA ON TIPOCONTA.IDTIPOCONTA = CONTAS.TIPOCONTA_IDTIPOCONTA WHERE SALDOINICIAL < 3000 AND UF = 'CE' AND DESCRIÇÃO <> 'CONTA CORRENTE'";
 
-let mySqlStringSplitted = teste.split(" ")
+let mySqlStringSplitted = teste.split(' ');
 
-let bodies = {}
+let bodies = {};
 
 let relationalAlgebraStrings = {
-    "SELECT": function (body) {
-        return `π ${body}`
-    },
-    "JOIN": function (body, fromBody) {
+	SELECT: function (body) {
+		return `π ${body}`;
+	},
+	JOIN: function (body, fromBody) {
+		let relationalBodyString = `${fromBody} ⋈ ${body['ON'][0]} ${body['JOIN'][0]}`;
+		delete body['ON'][0];
+		delete body['JOIN'][0];
 
-        let relationalBodyString = `${fromBody} ⋈ ${body["ON"][0]} ${body["JOIN"][0]}`
-        delete body["ON"][0]
-        delete body["JOIN"][0]
+		body['JOIN'].forEach((joinValue, index) => {
+			const onValue = body['ON'][index];
 
-        body["JOIN"].forEach((joinValue, index) => {
-            const onValue = body["ON"][index];
+			relationalBodyString = `${relationalBodyString} ⋈ ${onValue} ${joinValue}`;
+		});
 
-            relationalBodyString = `${relationalBodyString} ⋈ ${onValue} ${joinValue}`
-        });
+		return `(${relationalBodyString})`;
+	},
+	WHERE: function (body) {
+		return `σ ${body} `;
+	}
+};
 
-        return `(${relationalBodyString})`
-    },
-    "WHERE": function (body) {
-        return `σ ${body} `
-    }
-}
-
-let auxBody = ""
-let keyWords = ["SELECT", "FROM", "JOIN", "ON", "WHERE"]
-let auxKeyWord = ""
+let auxBody = '';
+let keyWords = ['SELECT', 'FROM', 'JOIN', 'ON', 'WHERE'];
+let auxKeyWord = '';
 
 for (let word of mySqlStringSplitted) {
-    if (keyWords.includes(word)) {
-        let auxBodyArray = bodies[auxKeyWord] == undefined ? [] : bodies[auxKeyWord]
-        auxBodyArray.push(auxBody.trim())
+	if (keyWords.includes(word)) {
+		let auxBodyArray =
+			bodies[auxKeyWord] == undefined ? [] : bodies[auxKeyWord];
+		auxBodyArray.push(auxBody.trim());
 
-        bodies[auxKeyWord] = auxBodyArray
+		bodies[auxKeyWord] = auxBodyArray;
 
-        auxKeyWord = word
-        auxBody = ""
-    } else {
-        auxBody += word + " "
-    }
+		auxKeyWord = word;
+		auxBody = '';
+	} else {
+		auxBody += word + ' ';
+	}
 }
 
-bodies[auxKeyWord] = auxBody.trim()
+bodies[auxKeyWord] = auxBody.trim();
 
 Object.entries(bodies).forEach(([key, value]) => {
-    if (key != "JOIN" && key != "ON" && key != "WHERE") {
-        bodies[key] = value[0]
-    }
+	if (key != 'JOIN' && key != 'ON' && key != 'WHERE') {
+		bodies[key] = value[0];
+	}
 });
 
-delete bodies[""]
+delete bodies[''];
 
 function printTest() {
-    console.log(bodies)
-    console.log()
-    console.log(relationalAlgebraStrings["SELECT"](bodies["SELECT"]))
-    console.log(relationalAlgebraStrings["WHERE"](bodies["WHERE"]))
+	console.log(bodies);
+	console.log();
+	console.log(relationalAlgebraStrings['SELECT'](bodies['SELECT']));
+	console.log(relationalAlgebraStrings['WHERE'](bodies['WHERE']));
 
-    let joinAux = { "JOIN": bodies["JOIN"], "ON": bodies["ON"] }
+	let joinAux = { JOIN: bodies['JOIN'], ON: bodies['ON'] };
 
-    console.log(relationalAlgebraStrings["JOIN"](joinAux, bodies["FROM"]))
+	console.log(relationalAlgebraStrings['JOIN'](joinAux, bodies['FROM']));
 }
 
-printTest()
+printTest();
 
 //console.log(1)
