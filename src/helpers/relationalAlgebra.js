e31e7eab58eb9eae26705bf1c8fc511241d79c49
Igor Oliveira dos Santos <49242732+Igorsant@users.noreply.github.com>
2022-05-05 22:36:26 -0300

    building graphs

--- src/helpers/relationalAlgebra.js
+++ src/helpers/relationalAlgebra.js
@@ -1,75 +1,122 @@
-const KEYWORDS = ['SELECT', 'FROM', 'JOIN', 'ON', 'WHERE']
+class TreeOptimizer{
+    constructor(query){
+        this.leaves = []
+        this.leaves.push(new No("", "", query.FROM))
+        console.log(query.JOIN)
+        for(let table of query.JOIN){
+            let aux = new No("", "", table)
+            
+            this.leaves.push(aux)
+        }
+    }
 
-export function splitQueryIntoBodies(query) {
-	let mySqlStringSplitted = query.split(' ')
+    buildTree(query){
 
-	let bodies = {}
-	let auxBody = ''
-	let auxKeyWord = ''
+    }
 
-	for (let word of mySqlStringSplitted) {
-		if (KEYWORDS.includes(word.toUpperCase())) {
-			let auxBodyArray =
-				bodies[auxKeyWord] == undefined ? [] : bodies[auxKeyWord]
-			auxBodyArray.push(auxBody.trim())
+    printLeaves(){
+        for(let leave of this.leaves){
+            console.log(leave, "-->")
+        }
+    }
+}
 
-			bodies[auxKeyWord] = auxBodyArray
 
-			auxKeyWord = word
-			auxBody = ''
-		} else {
-			auxBody += word + ' '
-		}
-	}
+class No{
+    constructor(selecao, projecao, name){
+        this.aresta = new Aresta(selecao, projecao)
+        this.name = name
+    }
 
-	bodies[auxKeyWord] = auxBody.trim()
+    setPai(no){
+        this.pai = no
+    }
+}
 
-	Object.entries(bodies).forEach(([key, value]) => {
-		if (!(key.toUpperCase() in ['JOIN', 'ON', 'WHERE'])) {
-			bodies[key] = value[0]
-		}
-	})
-
-	delete bodies['']
-
-	return bodies
+class Aresta{
+    constructor(selecao, projecao){
+        this.selecao = selecao
+        this.projecao = projecao
+    }
 }
 
 
 
-// const relationalAlgebraStrings = {
-// 	SELECT: function (body) {
-// 		return `π ${body}`
-// 	},
-// 	JOIN: function (body, fromBody) {
-// 		let relationalBodyString = `${fromBody} ⋈ ${body['ON'][0]} ${body['JOIN'][0]}`
-// 		delete body['ON'][0]
-// 		delete body['JOIN'][0]
+const teste =
+	"SELECT IDUSUARIO, NOME, DATANASCIMENTO, DESCRICAO, SALDOINICIAL, UF, DESCRIÇÃO FROM USUARIO JOIN CONTAS ON USUARIO.IDUSUARIO = CONTAS.USUARIO_IDUSUARIO JOIN TIPOCONTA ON TIPOCONTA.IDTIPOCONTA = CONTAS.TIPOCONTA_IDTIPOCONTA WHERE SALDOINICIAL < 3000 AND UF = 'CE' AND DESCRIÇÃO <> 'CONTA CORRENTE'";
 
-// 		body['JOIN'].forEach((joinValue, index) => {
-// 			const onValue = body['ON'][index]
+let mySqlStringSplitted = teste.split(' ');
 
-// 			relationalBodyString = `${relationalBodyString} ⋈ ${onValue} ${joinValue}`
-// 		})
+let bodies = {};
 
-// 		return `(${relationalBodyString})`
-// 	},
-// 	WHERE: function (body) {
-// 		return `σ ${body} `
-// 	}
-// }
+let relationalAlgebraStrings = {
+	SELECT: function (body) {
+		return `π ${body}`;
+	},
+	JOIN: function (body, fromBody) {
+		let relationalBodyString = `${fromBody} ⋈ ${body['ON'][0]} ${body['JOIN'][0]}`;
+		//delete body['ON'][0];
+		//delete body['JOIN'][0];
 
-// function printTest() {
-// 	console.log(bodies)
-// 	console.log()
-// 	console.log(relationalAlgebraStrings['SELECT'](bodies['SELECT']))
-// 	console.log(relationalAlgebraStrings['WHERE'](bodies['WHERE']))
+		body['JOIN'].forEach((joinValue, index) => {
+			const onValue = body['ON'][index];
 
-// 	let joinAux = { JOIN: bodies['JOIN'], ON: bodies['ON'] }
+			relationalBodyString = `${relationalBodyString} ⋈ ${onValue} ${joinValue}`;
+		});
 
-// 	console.log(relationalAlgebraStrings['JOIN'](joinAux, bodies['FROM']))
-// }
+		return `(${relationalBodyString})`;
+	},
+	WHERE: function (body) {
+		return `σ ${body} `;
+	}
+};
 
-// printTest()
+let auxBody = '';
+let keyWords = ['SELECT', 'FROM', 'JOIN', 'ON', 'WHERE'];
+let auxKeyWord = '';
 
-//console.log(1)
+for (let word of mySqlStringSplitted) {
+	if (keyWords.includes(word)) {
+		let auxBodyArray =
+			bodies[auxKeyWord] == undefined ? [] : bodies[auxKeyWord];
+		auxBodyArray.push(auxBody.trim());
+
+		bodies[auxKeyWord] = auxBodyArray;
+
+		auxKeyWord = word;
+		auxBody = '';
+	} else {
+		auxBody += word + ' ';
+	}
+}
+
+bodies[auxKeyWord] = auxBody.trim();
+
+Object.entries(bodies).forEach(([key, value]) => {
+	if (key != 'JOIN' && key != 'ON' && key != 'WHERE') {
+		bodies[key] = value[0];
+	}
+});
+
+delete bodies[''];
+
+function printTest() {
+	console.log(bodies);
+	console.log();
+	console.log(relationalAlgebraStrings['SELECT'](bodies['SELECT']));
+	console.log(relationalAlgebraStrings['WHERE'](bodies['WHERE']));
+
+	let joinAux = { JOIN: bodies['JOIN'], ON: bodies['ON'] };
+
+	console.log(relationalAlgebraStrings['JOIN'](joinAux, bodies['FROM']));
+
+  
+}
+
+printTest();
+console.log("----------------------------------------------------------------")
+console.log(bodies)
+const tree = new TreeOptimizer(bodies)
+console.log("leaves: ", tree.printLeaves())
+
+
