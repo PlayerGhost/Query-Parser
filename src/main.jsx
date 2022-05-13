eba01f1468f136a5c0bf8ee106252d48c4753aa6
Player Ghost <sousaitalo12@gmail.com>
2022-05-13 06:28:16 -0300

    fix reactDom import and render sintax cause of downgrade to react v17.0.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react"
+//import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom"
+import App from "./App"
+import "./index.css"
 
-createRoot(document.getElementById('root')).render(
+/*ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
-);
+)*/
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+)
