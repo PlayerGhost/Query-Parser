04291cacee36877f060dcf947d2e81825a063da5
Player Ghost <sousaitalo12@gmail.com>
2022-05-17 19:49:45 -0300

    fix graphDiv visibility

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,103 +1,106 @@
-import { useState } from 'react';
+import { useState } from "react"
 
-import { removeWhitespaces } from '../helpers/text-parser';
-import { regex } from '../helpers/regex';
-import { generateGraphToPlot } from '../helpers/relationalAlgebra.js';
+import { removeWhitespaces } from "../helpers/text-parser"
+import { regex } from "../helpers/regex"
+import { generateGraphToPlot } from "../helpers/relationalAlgebra.js"
 // import ZingChart from 'zingchart-react';
-import 'zingchart/es6';
-import 'zingchart/modules-es6/zingchart-tree.min.js';
+import "zingchart/es6"
+import "zingchart/modules-es6/zingchart-tree.min.js"
 
-import './MainPage.css';
+import "./MainPage.css"
 
-const ENTER_KEYCODE = 13;
-const ERROR_MESSAGE_TIMEOUT = 3000;
+const ENTER_KEYCODE = 13
+const ERROR_MESSAGE_TIMEOUT = 3000
 
 export default function MainPage() {
-	const [query, setQuery] = useState('');
-	const [error, setError] = useState('');
+	const [query, setQuery] = useState("")
+	const [error, setError] = useState("")
 	//const [ok, setOk] = useState('');
 
 	const onClick = () => {
 		if (!query) {
-			showErrorMessage('Sem query!');
-			return;
+			showErrorMessage("Sem query!")
+			return
 		}
 
-		const userQuery = query.replaceAll('\n', ' ').toUpperCase().trim();
+		const userQuery = query.replaceAll("\n", " ").toUpperCase().trim()
 
 		if (!userQuery.match(regex)) {
-			console.warn('Query filtrada', userQuery);
-			showErrorMessage('Query não passou no regex de verificação inicial!');
-			return;
+			console.warn("Query filtrada", userQuery)
+			showErrorMessage("Query não passou no regex de verificação inicial!")
+			return
 		}
 
-		const filteredQuery = removeWhitespaces(userQuery);
-		let graphTree;
+		const filteredQuery = removeWhitespaces(userQuery)
+		let graphTree
 		try {
-			graphTree = generateGraphToPlot(filteredQuery.replaceAll(';', ''));
+			graphTree = generateGraphToPlot(filteredQuery.replaceAll(";", ""))
 		} catch (err) {
-			console.log(err);
-			showErrorMessage('Houve algum erro gerando a árvore!');
+			console.log(err)
+			showErrorMessage("Houve algum erro gerando a árvore!")
 		}
 
 		let chartConfig = {
-			type: 'tree',
+			type: "tree",
 
 			options: {
-				aspect: 'tree-down',
+				aspect: "tree-down",
 				orgChart: true,
 
 				link: {
-					aspect: 'line'
+					aspect: "line",
 				},
 
 				node: {
-					backgroundColor: 'rgba(0,0,0,0)',
-					width: '100%',
-					height: '100%',
+					backgroundColor: "rgba(0,0,0,0)",
+					width: "100%",
+					height: "100%",
 
 					hoverState: {
-						visible: false
+						visible: false,
 					},
 
 					label: {
-						fontSize: '12px',
-						color: '#000000'
-					}
-				}
+						fontSize: "12px",
+						color: "#000000",
+					},
+				},
 			},
 
 			plot: {
 				exact: true,
 				smartSampling: true,
 				maxNodes: 0,
-				maxTrackers: 0
+				maxTrackers: 0,
 			},
 
 			plotarea: {
-				margin: '30px 85px'
+				margin: "30px 85px",
 			},
 
-			series: graphTree
-		};
+			series: graphTree,
+		}
 
 		zingchart.render({
-			id: 'graphDiv',
+			id: "graphDiv",
 			data: chartConfig,
 			cache: {
-				data: false
-			}
+				data: false,
+			},
 			// height: '100%',
 			// width: '100%',
 			// output: 'canvas'
-		});
-	};
+		})
+
+		document.getElementById("graphDiv").style.display = "block"
+	}
 
 	const showErrorMessage = (message) => {
-		console.error(message);
-		setError(message);
-		setTimeout(() => setError(''), ERROR_MESSAGE_TIMEOUT);
-	};
+		document.getElementById("graphDiv").style.display = "none"
+		console.error(message)
+		setError(message)
+		setTimeout(() => setError(""), ERROR_MESSAGE_TIMEOUT)
+	}
 
 	return (
 		<div className="main">
@@ -119,9 +122,9 @@ export default function MainPage() {
 				<div className="results">
 					<h2>Resultado da consulta</h2>
 					{Boolean(error) && <h3 className="error-message">Erro: {error}</h3>}
-					{ <div id="graphDiv" className="graph--container"></div>}
+					<div id="graphDiv" className="graph--container"></div>
 				</div>
 			</section>
 		</div>
-	);
+	)
 }
